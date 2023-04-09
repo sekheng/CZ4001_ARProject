1ac1fbc0f9edeaa692a8af5f599a5d2c151d9e54
Lee Jun Heng <leejunheng@gmail.com>
2023-04-09 22:13:54 +0800

    Minor changes to change scene
    Minor changes to change scene

--- Assets/Scripts/UI/ChangeScene.cs
+++ Assets/Scripts/UI/ChangeScene.cs
@@ -4,9 +4,14 @@ using UnityEngine.SceneManagement;
 public class ChangeScene : MonoBehaviour
 {
     private string currentARScene = "";
+    
+    public LeftPanelScript leftPanelScript; // Add a reference to the LeftPanelScript
 
     public void LoadScene(string sceneName)
     {
+        // Hide the sliding UI panel
+        leftPanelScript.HidePanel();
+
         if (!string.IsNullOrEmpty(currentARScene))
         {
             SceneManager.UnloadSceneAsync(currentARScene);
