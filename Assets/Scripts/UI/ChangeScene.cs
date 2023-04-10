5cb358f8b0779342583d0b40c76ab311070e1250
Lee Jun Heng <leejunheng@gmail.com>
2023-04-10 17:18:32 +0800

    change scene fixed
    change scene fixed

--- Assets/Scripts/UI/ChangeScene.cs
+++ Assets/Scripts/UI/ChangeScene.cs
@@ -1,11 +1,19 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
     private string currentARScene = "";
-    
+
     public LeftPanelScript leftPanelScript; // Add a reference to the LeftPanelScript
+
+    private GameObject activeARSceneObject;
+
+    private void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
 
     public void LoadScene(string sceneName)
     {
@@ -14,10 +22,47 @@ public class ChangeScene : MonoBehaviour
 
         if (!string.IsNullOrEmpty(currentARScene))
         {
-            SceneManager.UnloadSceneAsync(currentARScene);
+            StartCoroutine(SwitchScene(currentARScene, sceneName));
+        }
+        else
+        {
+            SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+            currentARScene = sceneName;
+            CreateActiveARSceneObject(sceneName);
+        }
+    }
+
+    private IEnumerator SwitchScene(string unloadScene, string loadScene)
+    {
+        // Unload the previous scene
+        AsyncOperation unloadOperation = SceneManager.UnloadSceneAsync(unloadScene);
+        while (!unloadOperation.isDone)
+        {
+            yield return null;
         }
 
-        SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
-        currentARScene = sceneName;
+        Destroy(activeARSceneObject);
+
+        // Load the new scene
+        AsyncOperation loadOperation = SceneManager.LoadSceneAsync(loadScene, LoadSceneMode.Additive);
+        while (!loadOperation.isDone)
+        {
+            yield return null;
+        }
+
+        currentARScene = loadScene;
+        CreateActiveARSceneObject(loadScene);
     }
+
+    private void CreateActiveARSceneObject(string sceneName)
+    {
+        activeARSceneObject = new GameObject("ActiveARScene");
+        activeARSceneObject.AddComponent<ActiveARScene>().sceneName = sceneName;
+        DontDestroyOnLoad(activeARSceneObject);
+    }
+}
+
+public class ActiveARScene : MonoBehaviour
+{
+    public string sceneName;
 }
